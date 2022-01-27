92b4464871b0f339884d50ea19ac6eb6a2accd94
TamjidAhmed10 <57794102+TamjidAhmed10@users.noreply.github.com>
2022-01-27 20:02:46 +0600

    day 1

---
+++ Day_1/commentsinpython.py
@@ -0,0 +1,11 @@
+### this is a single line comment
+"""this is a multi line comments
+this is a multi line comments
+this is a multi line comments"""
+
+def main():
+    """this is a main function which is documented for future use thank you"""
+
+if __name__ == "__main__":
+    print(main.__doc__)
+    main()
