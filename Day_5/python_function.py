2199d0dd0cd286968aac5e89ef49a24a2277e0b3
TamjidAhmed10 <57794102+TamjidAhmed10@users.noreply.github.com>
2022-02-03 18:43:10 +0600

    done

---
+++ Day_5/python_function.py
@@ -0,0 +1,6 @@
+def call_a_func():
+    print("hello from user define function")
+def main():
+    call_a_func()
+if __name__ == "__main__":
+    main()
---
+++ Day_5/python_passing_arguments.py
@@ -0,0 +1,9 @@
+def call_a_function_with_params(x):
+    print("hello there" , x)
+
+def main():
+    x=input("Enter a name \n")
+    call_a_function_with_params(x)
+
+if __name__ == "__main__":
+    main()
