edd06714b39ba417fb3e5f5463469ebdec228c8e
TamjidAhmed10 <57794102+TamjidAhmed10@users.noreply.github.com>
2022-02-05 10:57:44 +0600

    l

--- Day_6/class_examples.py
+++ Day_6/class_examples.py
@@ -1,10 +1,12 @@
-
 class Parrot:
     species="birds"
     def __init__(self , name , age):
         self.name=name
         self.age=age
 
+blue = Parrot("tamjid",4)
+
+print(blue.species)
 
 
         
